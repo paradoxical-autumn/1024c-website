675cfa29b382200a9bd7d71de161c5871b2757ce
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-11-27 22:24:33 +0000

    fix links, add 403 oh and a temporary favicon

--- docs/scripts/random-txt.js
+++ docs/scripts/random-txt.js
@@ -1,4 +1,12 @@
-var random_text_array = ["<i>yawn</i>", "hey how u doin'?", "badger badger badger badger", "erm... what der flip?", "sample text!", "autumn was here.", "<code>ParadoxicalError: Task failed successfully!</code>"]
+var random_text_array = ["<i>yawn</i>",
+    "hey how u doin'?",
+    "badger badger badger badger",
+    "erm... what der flip?",
+    "sample text!",
+    "autumn was here.",
+    "<code>ParadoxicalError: Task failed successfully!</code>",
+    "<a href=\"https://http.cat/\" target=\"_blank\" rel=\"noopener noreferrer\">funny cars.</a>"
+]
 
 window.addEventListener('load', getRandomTxt)
 
